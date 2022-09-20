0d8f282de68f731964771ae75bb3dc3d339320c0
Satwika7 <113924355+Satwika7@users.noreply.github.com>
2022-09-20 08:45:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#print(type(two_digit_number))
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+Final_value = int(first_digit)+int(second_digit)
+print(Final_value)
+
+
+
+
+
+
